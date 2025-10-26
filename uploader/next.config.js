42816d1d97e963337ee93e6ab03593c08e8bd490
Cliff Choiniere <crcnum4@gmail.com>
2025-10-26 16:36:04 -0400

    app runner creds

--- uploader/next.config.js
+++ uploader/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['mongoose']
+  serverExternalPackages: ['mongoose'],
+  output: 'standalone'
 }
 
 module.exports = nextConfig
